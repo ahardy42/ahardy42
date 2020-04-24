376a09cd574372455f9814836fb15dc68450b997
Andy Hardy <aohardy@gmail.com>
2020-04-24 15:43:50 -0600

    adding dev branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/contact' component={Contact}/>
+        <Route path='/projects' component={Projects}/>
+        <Route exact path='/' component={Home}/>
+        <Route path='*' component={NotFound}/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface LayoutProps {
+    headerVisible: boolean
+}
+
+export default function Layout({ headerVisible }: LayoutProps) {
+    return(
+        <div>Hey!</div>
+    )
+}
